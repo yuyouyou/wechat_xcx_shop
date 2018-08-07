21a5c37e236873d47becb2dba647199fcd75d11f
余双 <yushuang@zbj.com>
2018-08-07 17:32:37 +0800

    Feature：微信小程序商城

---
+++ pages/cart/cart.js
@@ -0,0 +1,336 @@
+var app = getApp();
+// pages/cart/cart.js
+Page({
+  data:{
+    page:1,
+    minusStatuses: ['disabled', 'disabled', 'normal', 'normal', 'disabled'],
+    total: 0,
+    carts: [],
+    startX: 0, //开始坐标
+    startY: 0
+  },
+  onLoad: function () {
+    this.loadProductData();
+    this.sum();
+  },
+  //手指触摸动作开始 记录起点X坐标
+  touchstart: function (e) {
+    //开始触摸时 重置所有删除
+    this.data.carts.forEach(function (v, i) {
+      if (v.isTouchMove)//只操作为true的
+        v.isTouchMove = false;
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      carts: this.data.carts
+    })
+  },
+  //滑动事件处理
+  touchmove: function (e) {
+    var that = this,
+      index = e.currentTarget.dataset.index,//当前索引
+      startX = that.data.startX,//开始X坐标
+      startY = that.data.startY,//开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+      //获取滑动角度
+      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+    that.data.carts.forEach(function (v, i) {
+      v.isTouchMove = false
+      //滑动超过30度角 return
+      if (Math.abs(angle) > 30) return;
+      if (i == index) {
+        if (touchMoveX > startX) //右滑
+          v.isTouchMove = false
+        else //左滑
+          v.isTouchMove = true
+      }
+    })
+    //更新数据
+    that.setData({
+      carts: that.data.carts
+    })
+  },
+  /**
+   * 计算滑动角度
+   * @param {Object} start 起点坐标
+   * @param {Object} end 终点坐标
+   */
+  angle: function (start, end) {
+    var _X = end.X - start.X,
+      _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+  },
+bindMinus: function(e) {
+    var that = this;
+    var index = parseInt(e.currentTarget.dataset.index);
+    var num = that.data.carts[index].num;
+    // 如果只有1件了，就不允许再减了
+    if (num > 1) {
+      num --;
+    } else {
+      wx.showToast({
+        title: '请左滑删除！',
+        icon: 'none',
+        duration: 2000
+      });
+      return false;
+    }
+    console.log(num);
+    var cart_id = e.currentTarget.dataset.cartid;
+    wx.request({
+      url: app.d.ceshiUrl + '/Api/Shopping/up_cart',
+      method:'post',
+      data: {
+        user_id: app.d.userId,
+        num:num,
+        cart_id:cart_id
+      },
+      header: {
+        'Content-Type':  'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var status = res.data.status;
+        if(status==1){
+          // 只有大于一件的时候，才能normal状态，否则disable状态
+          var minusStatus = num <= 1 ? 'disabled' : 'normal';
+          // 购物车数据
+          var carts = that.data.carts;
+          carts[index].num = num;
+          // 按钮可用状态
+          var minusStatuses = that.data.minusStatuses;
+          minusStatuses[index] = minusStatus;
+          // 将数值与状态写回
+          that.setData({
+            minusStatuses: minusStatuses
+          });
+          that.sum();
+        }else{
+          wx.showToast({
+            title: '操作失败！',
+            icon: 'none',
+            duration: 2000
+          });
+        }
+      },
+      fail: function() {
+        // fail
+        wx.showToast({
+          title: '网络异常！',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+},
+
+bindPlus: function(e) {
+    var that = this;
+    var index = parseInt(e.currentTarget.dataset.index);
+    var num = that.data.carts[index].num;
+    // 自增
+    num ++;
+    console.log(num);
+    var cart_id = e.currentTarget.dataset.cartid;
+    wx.request({
+      url: app.d.ceshiUrl + '/Api/Shopping/up_cart',
+      method:'post',
+      data: {
+        user_id: app.d.userId,
+        num:num,
+        cart_id:cart_id
+      },
+      header: {
+        'Content-Type':  'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var status = res.data.status;
+        if(status==1){
+          // 只有大于一件的时候，才能normal状态，否则disable状态
+          var minusStatus = num <= 1 ? 'disabled' : 'normal';
+          // 购物车数据
+          var carts = that.data.carts;
+          carts[index].num = num;
+          // 按钮可用状态
+          var minusStatuses = that.data.minusStatuses;
+          minusStatuses[index] = minusStatus;
+          // 将数值与状态写回
+          that.setData({
+            minusStatuses: minusStatuses
+          });
+          that.sum();
+        }else{
+          wx.showToast({
+            title: '操作失败！',
+            icon: 'none',
+            duration: 2000
+          });
+        }
+      },
+      fail: function() {
+        // fail
+        wx.showToast({
+          title: '网络异常！',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+},
+
+bindCheckbox: function(e) {
+  /*绑定点击事件，将checkbox样式改变为选中与非选中*/
+  //拿到下标值，以在carts作遍历指示用
+  var index = parseInt(e.currentTarget.dataset.index);
+  //原始的icon状态
+  var selected = this.data.carts[index].selected;
+  var carts = this.data.carts;
+  // 对勾选状态取反
+  carts[index].selected = !selected;
+  // 写回经点击修改后的数组
+  this.setData({
+    carts: carts
+  });
+  this.sum()
+},
+
+bindSelectAll: function() {
+   // 环境中目前已选状态
+   var selectedAllStatus = this.data.selectedAllStatus;
+   // 取反操作
+   selectedAllStatus = !selectedAllStatus;
+   // 购物车数据，关键是处理selected值
+   var carts = this.data.carts;
+   // 遍历
+   for (var i = 0; i < carts.length; i++) {
+     carts[i].selected = selectedAllStatus;
+   }
+   this.setData({
+     selectedAllStatus: selectedAllStatus,
+     carts: carts
+   });
+   this.sum()
+ },
+
+bindCheckout: function() {
+   // 初始化toastStr字符串
+   var toastStr = '';
+   // 遍历取出已勾选的cid
+   for (var i = 0; i < this.data.carts.length; i++) {
+     if (this.data.carts[i].selected) {
+       toastStr += this.data.carts[i].id;
+       toastStr += ',';
+     }
+   }
+   if (toastStr==''){
+     wx.showToast({
+       title: '选择结算商品!',
+       icon: 'none',
+       duration: 2000
+     });
+     return false;
+   }
+   //存回data
+   wx.navigateTo({
+     url: '../order/pay?cartId=' + toastStr,
+   })
+ },
+
+ bindToastChange: function() {
+   this.setData({
+     toastHidden: true
+   });
+ },
+
+sum: function() {
+    var carts = this.data.carts;
+    // 计算总金额
+    var total = 0;
+    for (var i = 0; i < carts.length; i++) {
+      if (carts[i].selected) {
+        total += carts[i].num * carts[i].price;
+      }
+    }
+    // 写回经点击修改后的数组
+    this.setData({
+      carts: carts,
+      total: '¥ ' + total
+    });
+  },
+
+onShow:function(){
+  this.loadProductData();
+},
+removeShopCard:function(e){
+    var that = this;
+    var cardId = e.currentTarget.dataset.cartid;
+    wx.showModal({
+      title: '提示',
+      content: '你确认移除吗',
+      success: function(res) {
+        res.confirm && wx.request({
+          url: app.d.ceshiUrl + '/Api/Shopping/delete',
+          method:'post',
+          data: {
+            cart_id: cardId,
+          },
+          header: {
+            'Content-Type':  'application/x-www-form-urlencoded'
+          },
+          success: function (res) {
+            //--init data
+            var data = res.data;
+            if(data.status == 1){
+              //that.data.productData.length =0;
+              that.loadProductData();
+            }else{
+              wx.showToast({
+                title: '操作失败！',
+                icon: 'none',
+                duration: 2000
+              });
+            }
+          },
+        });
+      },
+      fail: function() {
+        // fail
+        wx.showToast({
+          title: '网络异常！',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+  },
+
+// 数据案例
+  loadProductData:function(){
+    var that = this;
+    wx.request({
+      url: app.d.ceshiUrl + '/Api/Shopping/index',
+      method:'post',
+      data: {
+        user_id: app.d.userId
+      },
+      header: {
+        'Content-Type':  'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        //--init data
+        var cart = res.data.cart;
+        for (var i = 0; i < cart.length; i++) {
+          cart[i].isTouchMove = false
+        }
+        that.setData({
+          carts: cart
+        })
+        that.sum();
+        //endInitData
+      },
+    });
+  },
+
+})
